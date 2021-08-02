92f4cbd2156bf0df32c80827c17742d3562b1ac2
Stéphane KATTOOR <stephane.kattoor@sakana.fr>
2021-08-02 16:31:07 +0200

    Fixing circular import error

--- provider/__init__.py
+++ provider/__init__.py
@@ -2,17 +2,17 @@ from __future__ import annotations
 
 from typing import Dict, Sequence
 
+import market_data_loader
 import portfolio
-from market_data_loader import MarketData
 
 
 class Provider:
-    def get_quote(self, symbol: str) -> Dict[str, MarketData]:
+    def get_quote(self, symbol: str) -> Dict[str, market_data_loader.MarketData]:
         pass
 
     def get_quotes(
         self, portfolio: portfolio.Portfolio
-    ) -> Sequence[Dict[str, MarketData]]:
+    ) -> Sequence[Dict[str, market_data_loader.MarketData]]:
         pass
 
     @property
